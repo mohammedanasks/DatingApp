062fa345077b8a7164635deccb526b90f0a0b5b8
mohammedanas <mohammedanas121.ks@gmail.com>
2023-01-22 15:58:55 -0800

    secton4

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,19 +1,38 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
 {
-    public class IdentityServiceExtension
+    public static class IdentityServiceExtension
     {
         
        public static IServiceCollection AddIdentityServiceExtensions(this IServiceCollection services,
        IConfiguration config)
        {
-          
 
-            return services;
-       } 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+            AddJwtBearer(options=>
+{
+             options.TokenValidationParameters=new TokenValidationParameters
+         {
+             ValidateIssuerSigningKey=true,
+             IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+             ValidateIssuer=false,
+              ValidateAudience=false
+          };
+            });
+
+        return services;
+       }
+ 
+
+
+            
+       
     }
 }
